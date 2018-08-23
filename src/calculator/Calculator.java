51b81a8262dceb81b2da1032989944ab6be30e5a
Andrej_Tesakov <gavguy3@gmail.com>
2018-08-23 20:29:39 +0300

    calculate to 3 operation (/*-+)

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,5 +1,8 @@
 package calculator;
 
+import javax.print.DocFlavor;
+import java.sql.SQLOutput;
+
 /**
  * @author Dimitrijs Fedotovs <a href="http://www.bug.guru">www.bug.guru</a>
  * @version 1.0
@@ -8,7 +11,53 @@ package calculator;
 class Calculator {
 
     String calculate(String[] expression) {
-        return "0";
+        System.out.println(expression[0]);
+        System.out.println(expression[1]);
+        System.out.println(expression[2]);
+        System.out.println(expression[3]);
+        System.out.println(expression[4]);
+        double a = Double.parseDouble(expression[0]);
+        double b = Double.parseDouble(expression[2]);
+
+        double result;
+
+        switch (expression[1]) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                result = a / b;
+                break;
+            default:
+                return "error";
+        }
+        if (expression.length < 4) {
+
+            double c = Double.parseDouble(expression[4]); }
+        switch (expression[3]) {
+            case "+":
+                result = result + c;
+                break;
+            case "-":
+                result = result - c;
+                break;
+            case "*":
+                result = result * c;
+                break;
+            case "/":
+                result = result / c;
+                break;
+            default:
+                return "error";
+        }
+
+        return String.valueOf(result);
     }
 
 }
